8d9368d1784fb7540f7e5a406dba5e5fbbbf35fc
JC <jc@aequum.fr>
2015-04-22 12:19:56 +0200

    Add full screen picture for home slider

---
+++ web/resources/scripts/home.js
@@ -0,0 +1,34 @@
+
+$(document).ready(function() {
+
+    $('#banner-list').slick({
+        autoplay: true,
+        centerMode : true,
+        centerPadding : '0px',
+        dots : true,
+        fade : true,
+        nextArrow: '<span class="slick-next"></span>',
+        prevArrow: '<span class="slick-prev"></span>',
+        slide : 'li',
+        slidesToShow : 1,
+        speed: 500
+    });
+
+    $('#banner-list img').on('click', function() {
+
+        // Display full screen picture as popup
+        $('#home-popup').bPopup({
+            content: 'image',
+            loadUrl: $(this).attr('src')
+        });
+
+        // Close popup when clicking on picture
+        // NOTE : Clear popup content to avoid multiple content when re-open...
+        $('#home-popup').on('click', function() {
+            $('#home-popup').bPopup().close();
+            $('#home-popup').html('');
+        })
+
+        return false;
+    });
+});
