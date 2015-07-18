997e2d9df0c3d51129528b578332734114613669
JC <jc@aequum.fr>
2015-07-18 11:30:26 +0200

    Add fullscreen button on slideshow + remove userfiles from GIT repository

--- web/resources/scripts/section.js
+++ web/resources/scripts/section.js
@@ -61,28 +61,39 @@ function initThumbnailAction() {
 }
 
 /**
- * Allows to initialize action when user double click on picture with fullscreen.
+ * Allows to initialize action and CSS for pictures with fullscreen.
  */
 function initFullScreenAction() {
 
-    $('img.picture-fullscreen').on('click', function() {
+    $('img.picture-fullscreen').each(function() {
 
-        // Add css style (useful to keep border if exist)
-        $('#section-popup').attr('style', $(this).attr('style'));
+        // Define action when user click on button
+        $(this).on('click', function() {
 
-        // Display full screen picture as popup
-        $('#section-popup').bPopup({
-            content: 'image',
-            loadUrl: $(this).attr('data-url')
+            // Add css style (useful to keep border if exist)
+            $('#section-popup').attr('style', $(this).attr('style'));
+
+            // Display full screen picture as popup
+            $('#section-popup').bPopup({
+                content: 'image',
+                loadUrl: $(this).attr('data-url')
+            });
+
+            // Close popup when clicking on picture
+            // NOTE : Clear popup content to avoid multiple content when re-open...
+            $('#section-popup').on('click', function() {
+                $('#section-popup').bPopup().close();
+                $('#section-popup').html('');
+            })
+
+            return false;
         });
 
-        // Close popup when clicking on picture
-        // NOTE : Clear popup content to avoid multiple content when re-open...
-        $('#section-popup').on('click', function() {
-            $('#section-popup').bPopup().close();
-            $('#section-popup').html('');
-        })
-
-        return false;
+        // Display button depending on picture size
+        // NOTE : picture container => height: 481px; width: 570px;
+        var bottomPosition = 481 - $(this).height();
+        var rightPosition = (570 - $(this).width()) / 2;
+        $(this).next('span.picture-button').css('bottom', bottomPosition);
+        $(this).next('span.picture-button').css('right', rightPosition);
     });
 }
